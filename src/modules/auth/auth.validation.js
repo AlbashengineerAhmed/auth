1ecbf306621618f9ecc22e8753acbabfac3ee20e
Ahmed Mohamed <albashengineerahmed@gmail.com>
2023-10-27 20:08:21 +0200

    validation age

--- src/modules/auth/auth.validation.js
+++ src/modules/auth/auth.validation.js
@@ -4,6 +4,7 @@ export const signupSchema = joi.object({
   firstName:joi.string().alphanum().required().min(3).max(10),
   lastName:joi.string().alphanum().required().min(3).max(10),
   email:joi.string().email().required(),
+  age: joi.number().required().min(15).max(100),
   password:joi.string().pattern(new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)).required(),
   cPassword:joi.string().valid(joi.ref('password')).required()
 }).required()
