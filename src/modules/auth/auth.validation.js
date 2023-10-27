6b9f37c4ae843fbbc84bf118c8bb89019cbc93f8
Ahmed Mohamed <albashengineerahmed@gmail.com>
2023-10-27 20:04:59 +0200

    update validation

--- src/modules/auth/auth.validation.js
+++ src/modules/auth/auth.validation.js
@@ -1,8 +1,8 @@
 import joi from 'joi'
 
 export const signupSchema = joi.object({
-  first_name:joi.string().alphanum().required().min(3).max(10),
-  last_name:joi.string().alphanum().required().min(3).max(10),
+  firstName:joi.string().alphanum().required().min(3).max(10),
+  lastName:joi.string().alphanum().required().min(3).max(10),
   email:joi.string().email().required(),
   password:joi.string().pattern(new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)).required(),
   cPassword:joi.string().valid(joi.ref('password')).required()
